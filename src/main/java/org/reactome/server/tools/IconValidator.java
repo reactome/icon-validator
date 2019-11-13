b2b273d65d0cda64547cc3e7819dc3ff25e0538f
Chuqiaoo <gongchuqiao@gmail.com>
2019-11-13 16:27:37 +0000

    add category list and check the value

--- src/main/java/org/reactome/server/tools/IconValidator.java
+++ src/main/java/org/reactome/server/tools/IconValidator.java
@@ -2,23 +2,25 @@ package org.reactome.server.tools;
 
 import com.martiansoftware.jsap.*;
 import org.reactome.server.tools.model.Icon;
-import org.reactome.server.tools.model.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class IconValidator {
 
     private static final Logger logger = LoggerFactory.getLogger(IconValidator.class.getName());
 
-    // /Users/chuqiao/Dev/Icons/LIB
+
+    private final static List<String> CATEGORIES = new ArrayList<String>(Arrays.asList("arrow", "cell element",  "cell type", "compound", "human tissue", "protein", "receptor", "transporter"));
+
+    // directory = Users/chuqiao/Dev/Icons/LIB
     public static void main(String[] args) throws Exception {
 
         SimpleJSAP jsap = new SimpleJSAP(
@@ -43,26 +45,16 @@ public class IconValidator {
 
         File fileToPath = new File(directory);
         File[] listOfAllFiles = fileToPath.listFiles();
-        List<File> xmlFiles = new ArrayList<File>();
-
 
         //Iterate all format files in folder
         for (File file : listOfAllFiles) {
             if (file.isFile() && file.getName().endsWith(".xml")) {
-                xmlFiles.add(file);
+               toObj(directory, file);
             }
-        }
-
-        //Iterate all xml files
-        for (File xmlFile: xmlFiles){
-            toObj(directory, xmlFile);
         }
     }
 
     public Icon toObj(String directory, File xmlFiles){
-
-
-        // File xmlFile = new File(directory, xmlFiles.get(0).getName());
 
         File xmlFile = new File(directory, xmlFiles.getName());
 
@@ -70,31 +62,39 @@ public class IconValidator {
 
         try {
 
-           /* Icon i = new Icon();
+            /* Icon i = new Icon();
             List<Person> persons = new ArrayList<Person>();
             persons.add(new Person("c","aaa",null, "gggg"));
             persons.add(new Person("a","bbbb",null, "ccccc"));
-            i.setPerson(persons);*/
+            i.setPerson(persons);
+
+            Marshaller jaxb = jaxbContext.createMarshaller();
+            jaxb.marshal(i, new File("icon.xml"));
+            */
 
             jaxbContext = JAXBContext.newInstance(Icon.class);
-
-           /*
-           Marshaller jaxb = jaxbContext.createMarshaller();
-            jaxb.marshal(i, new File("icon.xml"));*/
 
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 
             Icon icon = (Icon) jaxbUnmarshaller.unmarshal(xmlFile);
 
-            System.out.println(icon);
+            List<String> cat = icon.getCategories();
 
+            for (String item : cat) {
+                if(!CATEGORIES.contains(item.toLowerCase())){
+                    //Todo: category is not found in category list ot no items in categories tag
+                    logger.info("category is not correct in " + xmlFile.getName() );
+                }
+            }
+
+            System.out.println(icon);
+            // Todo
             return icon;
 
         } catch (JAXBException e) {
             e.printStackTrace();
         }
-
+        // Todo
         return null;
     }
-
 }
