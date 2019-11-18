dc01da6f82bc543554177e5bda89513e5024ad12
Chuqiaoo <gongchuqiao@gmail.com>
2019-11-18 17:26:07 +0000

    move error counter out of process method

--- src/main/java/org/reactome/server/tools/IconValidator.java
+++ src/main/java/org/reactome/server/tools/IconValidator.java
@@ -21,7 +21,7 @@ public class IconValidator {
     private static final Logger logger = LoggerFactory.getLogger("logger");
     private static final Logger errorLogger = LoggerFactory.getLogger("errorLogger");
 
-    private final static List<String> CATEGORIES = new ArrayList<String>(Arrays.asList("arrow", "cell_element",  "cell_type", "compound", "human_tissue", "protein", "receptor", "transporter"));
+    private final static List<String> CATEGORIES = new ArrayList<String>(Arrays.asList("arrow", "cell_element", "cell_type", "compound", "human_tissue", "protein", "receptor", "transporter"));
     private final static List<String> REFERENCES = new ArrayList<String>(Arrays.asList("UNIPROT", "GO", "CHEBI", "ENSEMBL", "CL", "UBERON", "INTERPRO", "MESH", "KEGG", "ENA", "SO", "BTO", "RFAM", "PUBCHEM", "PFAM", "COMPLEXPORTAL", "OMIT"));
 
     private int error = 0;
@@ -32,20 +32,22 @@ public class IconValidator {
         SimpleJSAP jsap = new SimpleJSAP(
                 IconValidator.class.getName(),
                 "Validates all the icon metadata before it is indexed during data release",
-                new Parameter[] {
-                         new FlaggedOption( "directory", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'd', "directory", "The place of icon XML s to import").setList(true).setListSeparator(',')
-                        , new FlaggedOption( "force", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.NOT_REQUIRED, 'f', "force", "force icon validator to execute ")
+                new Parameter[]{
+                        new FlaggedOption("directory", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'd', "directory", "The place of icon XML s to import").setList(true).setListSeparator(',')
+                        , new FlaggedOption("force", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.NOT_REQUIRED, 'f', "force", "force icon validator to execute ")
                 }
         );
 
         JSAPResult config = jsap.parse(args);
 
-        if(jsap.messagePrinted()) System.exit( 1 );
+        if (jsap.messagePrinted()) System.exit(1);
 
         IconValidator iv = new IconValidator();
         iv.process(config);
-        if (iv.error > 0) System.exit(1);
-
+        if (iv.getError() > 0) {
+            errorLogger.info(iv.getError() + " errors are found in " + iv.getXmlNum() + " XML files.");
+            System.exit(1);
+        }
     }
 
     private void process(JSAPResult config) {
@@ -59,19 +61,18 @@ public class IconValidator {
             }
         });
 
-        if (xmlFiles != null){
+        if (xmlFiles != null) {
             for (File file : xmlFiles) {
                 xmlNum = xmlFiles.length;
                 Icon icon = convertXmlToObj(file);
-                if( icon != null){
+                if (icon != null) {
                     validateXmlObj(file, icon);
                 }
             }
         }
-        errorLogger.error(error + " errors are found in " + xmlNum + " XML files");
     }
 
-    private Icon convertXmlToObj(File file){
+    private Icon convertXmlToObj(File file) {
 
         JAXBContext jaxbContext;
         try {
@@ -80,33 +81,33 @@ public class IconValidator {
             Icon icon = (Icon) jaxbUnmarshaller.unmarshal(file);
             return icon;
         } catch (JAXBException e) {
-            errorLogger.error(e.getCause().getMessage() +  " File: " + file.getName());
+            errorLogger.error(e.getCause().getMessage() + " File: " + file.getName());
             error++;
         }
         return null;
     }
 
-    private void validateXmlObj(File xmlFile, Icon icon){
+    private void validateXmlObj(File xmlFile, Icon icon) {
 
         List<String> categories = icon.getCategories();
         for (String category : categories) {
-            if(!CATEGORIES.contains(category.toLowerCase())){
+            if (!CATEGORIES.contains(category.toLowerCase())) {
                 errorLogger.error("Category [" + category + "] is not in the list CATEGORIES in the " + xmlFile.getName() + ".");
                 error++;
             }
         }
 
-        List<Person> person= icon.getPerson();
-        if(person == null){
+        List<Person> person = icon.getPerson();
+        if (person == null) {
             errorLogger.error("Person not found in " + xmlFile.getName() + ".");
             error++;
         }
 
         List<Reference> references = icon.getReferences();
-        if(references != null){
+        if (references != null) {
             for (Reference reference : references) {
-                if(!REFERENCES.contains(reference.getDb())){
-                    errorLogger.error("["+ reference.getDb() + "] is not in the list REFERENCE in " + xmlFile.getName() + ".");
+                if (!REFERENCES.contains(reference.getDb())) {
+                    errorLogger.error("[" + reference.getDb() + "] is not in the list REFERENCE in " + xmlFile.getName() + ".");
                     error++;
                 }
             }
@@ -115,13 +116,29 @@ public class IconValidator {
         }
 
         List<String> synonyms = icon.getSynonyms();
-        if(synonyms != null) {
+        if (synonyms != null) {
             for (String synonym : synonyms) {
-                if (synonym.equals("")){
+                if (synonym.equals("")) {
                     errorLogger.error("Synonym is missing value in " + xmlFile.getName() + ".");
                     error++;
                 }
             }
         }
+    }
+
+    public int getError() {
+        return error;
+    }
+
+    public void setError(int error) {
+        this.error = error;
+    }
+
+    public int getXmlNum() {
+        return xmlNum;
+    }
+
+    public void setXmlNum(int xmlNum) {
+        this.xmlNum = xmlNum;
     }
 }
