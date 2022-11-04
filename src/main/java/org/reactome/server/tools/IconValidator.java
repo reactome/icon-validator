971bd1d1d2b447918623f2f91d738094d55b5c7c
EliotRagueneau <eragueneau@ebi.ac.uk>
2022-11-04 16:27:59 +0000

    :bug: Support pubchem substances

--- src/main/java/org/reactome/server/tools/IconValidator.java
+++ src/main/java/org/reactome/server/tools/IconValidator.java
@@ -145,7 +145,7 @@ public class IconValidator implements Checker {
             "ENA", List.of("ena.embl"),
             "PUBCHEM", List.of("pubchem.compound"),
             "PUBCHEM-BIOASSAY", List.of("pubchem.bioassay"),
-            "PUBCHEM-SUBSTANCES", List.of("pubchem.substances"),
+            "PUBCHEM-SUBSTANCE", List.of("pubchem.substances"),
             "IUPHAR", List.of("iuphar.family", "iuphar.ligand", "iuphar.receptor"),
             "NCBI", List.of("ncbigene", "ncbiprotein")
     );
